2d83f33d3d9d42363e3a5aca81ebbd24f1731546
BlackCockder <makobi99@gmail.com>
2024-06-11 14:00:56 +0200

    Version approved by professor

--- semetralnezaliczenie/main.py
+++ semetralnezaliczenie/main.py
@@ -9,10 +9,10 @@ mass = 1
 pot_depth = 1
 sigma = 1
 T = 2.5
-dt = 0.00001
+dt = 0.001
 atoms = []
-kb = 1.38064852e-23
-atom_count = 16
+kb = 1
+atom_count = 36
 cutoff_distance = 6
 
 class Atom:
@@ -40,14 +40,14 @@ def uklad(i):
     plt.axes(xlim=(0, i), ylim=(0, i), aspect='equal')
     for y in range(isqrt):
         for x in range(isqrt):
-            atom = Atom(0.5*isqrt + x*isqrt, 0.5*isqrt + y*isqrt, round(random.uniform(-0.5, 0.5), 2), round(random.uniform(-0.5, 0.5), 2), 0.175*isqrt)
+            atom = Atom(0.5*isqrt + x*isqrt, 0.5*isqrt + y*isqrt, random.uniform(-0.5, 0.5), random.uniform(-0.5, 0.5), 0.175*isqrt)
             fig.gca().add_patch(atom.circle)
             atoms.append(atom)
             velocities.append(atom.velocity)
     sum_velocity = np.sum(np.array(velocities), axis=0)
-    for i, atom in enumerate(atoms):
+    for j, atom in enumerate(atoms):
         atom.velocity -= sum_velocity / len(velocities)
-        print(f"Temperatura atomu {i + 1} wynosi: " + str((mass*np.linalg.norm(atom.velocity)**2)/3*kb) + " K")
+        print(f"Temperatura atomu {j + 1} wynosi: " + str((mass*np.linalg.norm(atom.velocity)**2)/3*kb) + " K")
     return fig
 
 
@@ -55,10 +55,10 @@ def animate_instance(frame):
     global dt
     global atoms
     global mass
-    # Jako przyspieszanie obliczen uzylem metody hashmapy dla wartosci. Zmienia algorytm z O(n*(n-1)) w O((n*(n-1)/2)
+    global cutoff_distance
     cache = {}
     verletList = {}
-    for n in range(1000):
+    for n in range(100):
         if n % 10 == 0:
             verletList = {}
             for i, atom in enumerate(atoms):
@@ -81,8 +81,8 @@ def animate_instance(frame):
                     else:
                         rij = atom.position - atom_par.position
                         dij = np.linalg.norm(rij)
-                        sily[j] += 24 * rij / dij ** 2 * (2 * (1 / dij) ** 12 - (1 / dij) ** 6)
-                        cache[(j, i)] = sily[j]
+                        sily[j] -= 24 * rij / dij ** 2 * (2 * (1 / dij) ** 12 - (1 / dij) ** 6)
+                        cache[(j, i)] = -sily[j]
             atom.update_velocity(np.sum(sily, axis=0)/mass*dt)
             atom.update_position()
     return [atom.circle for atom in atoms]
