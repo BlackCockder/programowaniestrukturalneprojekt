96bc8c8b303890ed58df6cea178d52e6e429df88
BlackCockder <143130319+BlackCockder@users.noreply.github.com>
2025-01-28 23:00:13 +0100

    Commit for lab work

--- laby/graphs.py
+++ laby/graphs.py
@@ -3,6 +3,8 @@ import numpy as np
 from scipy.optimize import curve_fit as curve_fit
 from scipy.interpolate import interp1d as parmezan
 from scipy.optimize import fsolve as chrobry
+from sklearn.metrics import mean_squared_error
+
 
 
 
@@ -18,79 +20,82 @@ from scipy.optimize import fsolve as chrobry
 # def uncertainty_of_weighted_average(weights):
 #     uncertainty = (1 / sum(weights))**0.5
 #     return float(uncertainty)
+
+nazwy_parametrow = [0.12, 0.06, 0.04, 0.03, 0.024, 0.02, 0.017, 0.015, 0.013, 0.012, 0.011, 0.01, 0.0092]
+
+y_true = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65]
+
+
+# Plot
+plt.figure(figsize=(8, 5))
+plt.scatter(y_true, nazwy_parametrow, label="Zmierzona gęstość", color="blue")
+plt.plot(y_true, nazwy_parametrow, color="blue", linestyle="--", alpha=0.7)
+plt.title("Wykres zależności niepewności wzorcowania od ilości okrążeń wahadła")
+plt.ylabel("Niepewność wzorcowania [1/s]")
+plt.xlabel("Ilość okrążeń")
+plt.grid()
+plt.show()
+
+# def linear_fit(x, a, b):
+#     return a * x + b
+#
+# def poly_fit(x, a, b, c):
+#     return a * x**2 + b * x + c
 #
 #
-# nazwy_parametrow = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-# wykres_wartosci = [8.60, 9.09, 9.47, 9.64, 9.84, 9.78, 9.74, 9.72, 9.72]
-# wykres_niepewnosci_wartosci = [0.126, 0.126, 0.126, 0.126, 0.126, 0.126, 0.126, 0.126, 0.126]
+# L_nieruchome = np.array([40, 42, 44, 45, 46, 45.8, 45.6, 45.4, 45.5])
+# T_nieruchome = np.array([1.354, 1.35, 1.354, 1.357, 1.358, 1.359, 1.359, 1.357, 1.359])
+#
+# L_ruchome = np.array([40, 42, 44, 45, 46, 45.8, 45.6, 45.4, 45.2, 45.5])
+# T_ruchome = np.array([1.453, 1.389, 1.361, 1.349, 1.343, 1.344, 1.349, 1.348, 1.34, 1.35])
+#
+# params_linear, _ = curve_fit(linear_fit, L_nieruchome, T_nieruchome)
+# a_linear, b_linear = params_linear
+#
+# params_poly, _ = curve_fit(poly_fit, L_ruchome, T_ruchome)
+# a_poly, b_poly, c_poly = params_poly
+#
+# L_fit = np.linspace(min(min(L_nieruchome), min(L_ruchome)),
+#                     max(max(L_nieruchome), max(L_ruchome)), 500)
+#
+# T_fit_nieruchome = linear_fit(L_fit, *params_linear)
+# T_fit_ruchome = poly_fit(L_fit, *params_poly)
+#
+# # Difference between the two fitted functions
+# T_diff = T_fit_ruchome - T_fit_nieruchome
 #
 #
-# print(wykres_wartosci)
-# print(wykres_niepewnosci_wartosci)
+# def jozef_pilsudzki(L):
+#     return poly_fit(L, *params_poly) - linear_fit(L, *params_linear)
 #
-# # Plot
-# plt.figure(figsize=(8, 5))
-# plt.errorbar(nazwy_parametrow, wykres_wartosci, yerr=wykres_niepewnosci_wartosci, fmt='o', label="Side A", capsize=5)
-# plt.title("Wykres pomiarów g dla nieruchomej tulejki")
-# plt.ylabel("Wyliczone przyspieszenie ziemskie [m/s2]")
-# plt.xlabel("Numer pomiaru")
-# plt.grid()
+#
+# interp = parmezan(L_fit, T_diff)
+# L0 = chrobry(jozef_pilsudzki, x0=45)[0]
+# T0 = linear_fit(L0, *params_linear)
+#
+# plt.figure(figsize=(10, 6))
+#
+# plt.scatter(L_nieruchome, T_nieruchome, color='blue', label='T nieruchome', zorder=5)
+# plt.scatter(L_ruchome, T_ruchome, color='red', label='T ruchome', zorder=5)
+#
+# plt.plot(L_fit, T_fit_nieruchome, 'b--', label=f'Dopasowanie liniowe')
+# plt.plot(L_fit, T_fit_ruchome, 'g-', label='Dopasowanie kwadratowe')
+#
+# plt.scatter(L0, T0, color='purple', s=80, label=f'Intersection L0={L0:.2f}, T={T0:.2f}')
+#
+# # Labels and legend
+# plt.xlabel('L (cm)')
+# plt.ylabel('T (s)')
+# plt.title('Dopasowanie kwadratowe i liniowe dla T ruchomego i nieruchomego')
+# plt.legend()
+# plt.grid(True)
 # plt.show()
 
 
-
-
-def linear_fit(x, a, b):
-    return a * x + b
-
-def poly_fit(x, a, b, c):
-    return a * x**2 + b * x + c
-
-
-L_nieruchome = np.array([40, 42, 44, 45, 46, 45.8, 45.6, 45.4, 45.5])
-T_nieruchome = np.array([1.354, 1.35, 1.354, 1.357, 1.358, 1.359, 1.359, 1.357, 1.359])
-
-L_ruchome = np.array([40, 42, 44, 45, 46, 45.8, 45.6, 45.4, 45.2, 45.5])
-T_ruchome = np.array([1.453, 1.389, 1.361, 1.349, 1.343, 1.344, 1.349, 1.348, 1.34, 1.35])
-
-params_linear, _ = curve_fit(linear_fit, L_nieruchome, T_nieruchome)
-a_linear, b_linear = params_linear
-
-params_poly, _ = curve_fit(poly_fit, L_ruchome, T_ruchome)
-a_poly, b_poly, c_poly = params_poly
-
-L_fit = np.linspace(min(min(L_nieruchome), min(L_ruchome)),
-                    max(max(L_nieruchome), max(L_ruchome)), 500)
-
-T_fit_nieruchome = linear_fit(L_fit, *params_linear)
-T_fit_ruchome = poly_fit(L_fit, *params_poly)
-
-# Difference between the two fitted functions
-T_diff = T_fit_ruchome - T_fit_nieruchome
-
-
-def jozef_pilsudzki(L):
-    return poly_fit(L, *params_poly) - linear_fit(L, *params_linear)
-
-
-interp = parmezan(L_fit, T_diff)
-L0 = chrobry(jozef_pilsudzki, x0=45)[0]
-T0 = linear_fit(L0, *params_linear)
-
-plt.figure(figsize=(10, 6))
-
-plt.scatter(L_nieruchome, T_nieruchome, color='blue', label='T nieruchome', zorder=5)
-plt.scatter(L_ruchome, T_ruchome, color='red', label='T ruchome', zorder=5)
-
-plt.plot(L_fit, T_fit_nieruchome, 'b--', label=f'Dopasowanie liniowe')
-plt.plot(L_fit, T_fit_ruchome, 'g-', label='Dopasowanie kwadratowe')
-
-plt.scatter(L0, T0, color='purple', s=80, label=f'Intersection L0={L0:.2f}, T={T0:.2f}')
-
-# Labels and legend
-plt.xlabel('L (cm)')
-plt.ylabel('T (s)')
-plt.title('Dopasowanie kwadratowe i liniowe dla T ruchomego i nieruchomego')
-plt.legend()
-plt.grid(True)
-plt.show()
+# y_true = [0.997795, 0.997069, 0.995672, 0.994058, 0.992244, 0.990244, 0.985731]
+# y_pred = [0.839, 0.845, 0.848, 0.852, 0.845, 0.852, 0.838]
+#
+# relative_errors = np.abs((np.array(y_true) - np.array(y_pred)) / np.array(y_true)) * 100
+# mape = np.mean(relative_errors)
+#
+# print("Mean Absolute Percentage Error (MAPE):", mape, "%")
--- leak.py
+++ leak.py
@@ -1,9 +1,41 @@
-import requests
+import numpy as np
 
-def get_public_ip_address():
-    response = requests.get('https://api.ipify.org')
-    return response.text
+# values = {
+#     "MR_air": 0.0133,      # moment w powietrzu [N·m]
+#     "MR_ciecz": 0.0227,  # moment w cieczy [N·m]
+#     "rho_ref": 1.2,       # gęstość referencyjna w kg/m^3
+#     "uMR_air": 4.76 * (10**-5),    # niepewność momentu w powietrzu [N·m] 0.002
+#     "uMR_ciecz": 6.12 * (10**-5),  # niepewność momentu w cieczy [N·m] 0.001, 0.0000166
+# }
+#
+# # Wiktor
+# # values = {
+# #     "MR_air": 0.01478,      # moment w powietrzu [N·m]
+# #     "MR_ciecz": 0.0236,  # moment w cieczy [N·m]
+# #     "rho_ref": 1.2,       # gęstość referencyjna w kg/m^3
+# #     "uMR_air": 9.25 * (10**-5),    # niepewność momentu w powietrzu [N·m] 0.002
+# #     "uMR_ciecz": 7.33 * (10**-5),  # niepewność momentu w cieczy [N·m] 0.001, 0.0000166
+# # }
+#
+#
+# def rho_uncertainty(mrAir, mrFluid, pref, u_mrAir, u_mrFluid):
+#     return np.sqrt((pref/mrAir*u_mrFluid)**2+(pref*(-((mrFluid-mrAir)/mrAir**2)-(1/mrAir))*u_mrAir)**2) / 1000
+#
+#
+# print(rho_uncertainty(values["MR_air"], values["MR_ciecz"], values["rho_ref"], values["uMR_air"], values["uMR_ciecz"]))
 
-if __name__ == "__main__":
-    my_public_ip = get_public_ip_address()
-    print("My public IP address is:", my_public_ip)
+g = 9.81
+l = 0.01
+
+massArray = [1, 0, 0, 0, 1, 0, 0, 0, 1]
+
+
+def MR_uncertainty(massArray):
+    sumOfIndexes = []
+    for index, value in enumerate(massArray):
+        if value != 0:
+            sumOfIndexes.append(((index + 1)*0.058)**2)
+    return g*l*np.sqrt(sum(sumOfIndexes)) / 1000
+
+
+print(MR_uncertainty(massArray))
